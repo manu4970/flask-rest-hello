1fb0288d51f1092db46459adf816f97ebbe58cd3
Manuel Perez <66376102+manu4970@users.noreply.github.com>
2023-06-12 23:30:12 +0000

    se crea trabla favoritos

--- src/models.py
+++ src/models.py
@@ -3,17 +3,31 @@ from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-peopleFavs = db.Table("peopleFav",
-     db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
-     db.Column("people_id", db.Integer, db.ForeignKey("people.id"), primary_key=True)
-)
 
-planetsFavs = db.Table("planetsFav",
-     db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
-     db.Column("planets_id", db.Integer, db.ForeignKey("planets.id"), primary_key=True)
-)
-    
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "is_active": self.is_active,
+            # do not serialize the password, its a security breach
+        }
+
+
 class People(db.Model):
+    __tablename__ = "people"
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(120), nullable=False)
 
@@ -25,8 +39,10 @@ class People(db.Model):
             "id": self.id,
             "name": self.name,
         }
-    
+
+
 class Planets(db.Model):
+    __tablename__ = "planets"
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(120), nullable=False)
 
@@ -38,35 +54,25 @@ class Planets(db.Model):
             "id": self.id,
             "name": self.name,
         }
-    
 
-class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    username = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    peopleFav = db.relationship(People,
-                    secondary=peopleFavs,
-                    lazy='subquery',
-                    backref=db.backref('users', lazy=True))
-    planetsFav = db.relationship(Planets,
-                    secondary=planetsFavs,
-                    lazy='subquery',
-                    backref=db.backref('users', lazy=True))
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planeta_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    personaje_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    personaje = db.relationship("People")
+    planeta = db.relationship("Planets")
+    usuario = db.relationship("User")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Favorites %r>' % self.name
 
-    def serialize(self):
+    def serialize2(self):
         return {
             "id": self.id,
-            "email": self.email,
-            "username": self.username,
-            "is_active": self.is_active,
-            "people_fav": self.peopleFav,
-            "planet_fav": self.planetsFav
-            # do not serialize the password, its a security breach
+            "usuario_id": self.usuario_id,
+            "planeta_id": self.planeta_id,
+            "personaje_id": self.planeta_id,
         }
